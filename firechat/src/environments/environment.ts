cb78b9a258a6e079392198450555d59ff5c0e5f1
Jonas <jesusquiterio77@gmail.com>
2023-11-12 22:03:32 -0600

    video 5

---
+++ firechat/src/environments/environment.ts
@@ -0,0 +1,16 @@
+
+
+
+
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBXaLURfZg0hn1iNSGBVdhpf1WX0Q9EEcI",
+        authDomain: "firechat-b40e0.firebaseapp.com",
+        projectId: "firechat-b40e0",
+        storageBucket: "firechat-b40e0.appspot.com",
+        messagingSenderId: "98528117241",
+        appId: "1:98528117241:web:48d43983b110002ef63d28"
+      }
+}
